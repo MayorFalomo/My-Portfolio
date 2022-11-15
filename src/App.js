8b21df20e45ba84773437cf7bd7dc5867a68605e
MayorFalomo <mayorfalomo@gmailcom>
2022-11-15 14:33:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>OOps!, Nothing here, Click the link to check out my Portfolio </h1>
+    </div>
+  );
+};
+
+export default App;
